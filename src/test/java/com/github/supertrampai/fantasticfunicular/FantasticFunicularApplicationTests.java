a78c2e88c01a71c96191c662a7fbb25edca83c68
XHongLi <33442306+XHongLi@users.noreply.github.com>
2019-05-09 11:30:36 +0800

    第一次提交项目

---
+++ src/test/java/com/github/supertrampai/fantasticfunicular/FantasticFunicularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.supertrampai.fantasticfunicular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantasticFunicularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
