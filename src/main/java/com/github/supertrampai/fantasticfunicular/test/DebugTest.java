442d06691ad7fe553c59499089868ef1b5917e7d
SuperTrampAI <lxh800109@gmail.com>
2019-08-18 23:47:48 +0800

    test
    test debug

---
+++ src/main/java/com/github/supertrampai/fantasticfunicular/test/DebugTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017- 2019 SuperTrampAI.github All Rights Reserved.
+ */
+
+package com.github.supertrampai.fantasticfunicular.test;
+
+/**
+ * @author lxh800109@gmail.com
+ * @Description
+ * @create 2019/08/17 11:33
+ */
+public class DebugTest {
+
+    public static void main(String[] args) {
+       System.out.println("------------第一个循环开始---------------");
+       for (int i=0 ; i<10 ; i++){
+            System.out.println("i:add:"+i);
+       }
+
+        System.out.println("------------第er个循环开始---------------");
+        for (int j=0 ; j<10 ; j++){
+            System.out.println("j:"+j);
+        }
+
+        for(int k=0;k<10 ; k++){
+            System.out.println("k:"+k);
+        }
+
+    }
+
+}
